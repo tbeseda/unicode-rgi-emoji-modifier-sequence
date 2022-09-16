968decc0d66e538f01463e2f968e48810dffb09b
Taylor Beseda <tbeseda@gmail.com>
2022-09-16 13:32:01 -0600

    update tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,9 @@
-'use strict';
+import test from "node:test";
+import assert from "node:assert";
 
-const assert = require('assert');
-const emojiModifierBase = require('../index.js');
+import emojiModifierBase from '../index.js'
 
-describe('basic functionality', function() {
-	it('contains only the correct code points', function() {
+test('basic functionality', function() {
 		assert.equal(emojiModifierBase.has(0x261D), true);
 		assert.equal(emojiModifierBase.has(0x26F9), true);
 		assert.equal(emojiModifierBase.has(0x1F465), false);
@@ -13,5 +12,4 @@ describe('basic functionality', function() {
 		assert.equal(emojiModifierBase.has(0x1F469), true);
 		assert.equal(emojiModifierBase.has(0x1F46A), false);
 		assert.equal(emojiModifierBase.has(0x1F93E), true);
-	});
 });
